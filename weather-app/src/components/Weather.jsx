082adb7acc8e7c59965422e83a5b3f7f0b973265
Amir <amir.abouelhassan@ontariotechu.net>
2024-12-29 00:38:48 -0500

    Updated UI

--- weather-app/src/components/Weather.jsx
+++ weather-app/src/components/Weather.jsx
@@ -5,9 +5,9 @@ import search_icon from '../assets/search.png'
 const Weather = () => {
   return (
     <div className='weather'>
-      <div className='search-bar'>
+      <div className="search-bar">
         <input type="text" placeholder='Search'/>
-        <img src={search_icon} alt="" />
+        <img className="search-icon" src={search_icon} alt="Search Icon" />
       </div>
     </div>
   ) 
